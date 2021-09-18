87e8200cd22d5b7d243344c28fec348834db432e
yvs2701 <yashvardhan.ys86@gmail.com>
2021-09-19 01:22:44 +0530

    gh pages routes...

--- src/App.js
+++ src/App.js
@@ -9,31 +9,29 @@ import Video from './components/resource pages/Video'
 import Books from './components/resource pages/Books'
 import Career from './components/resource pages/Career'
 import Moral from './components/resource pages/Moral'
-import NotFound from './components/NotFound'
 
 function App() {
   return (
     <div className="App">
       <Switch>
-        <Route path='/' component={Home} exact />
-        <Route path='/fun-edu' component={Home} exact />
-
-        <Route path='/fiveToTen' component={FiveToTen} exact />
         <Route path='/fiveToTen/music' exact><Music age={10} /></Route>
         <Route path='/fiveToTen/video' exact><Video age={10} /></Route>
+        <Route path='/fiveToTen' component={FiveToTen} exact />
 
-        <Route path='/elevenToThirteen' component={ElevenToThirteen} exact />
         <Route path='/elevenToThirteen/music' exact ><Music age={13} /></Route>
         <Route path='/elevenToThirteen/video' exact ><Video age={13} /></Route>
         <Route path='/elevenToThirteen/books' exact ><Books age={13} /></Route>
+        <Route path='/elevenToThirteen' component={ElevenToThirteen} exact />
 
-        <Route path='/fourteenToEighteen/' component={FourteenToEighteen} exact />
         <Route path='/fourteenToEighteen/music' exact ><Music age={18} /></Route>
         <Route path='/fourteenToEighteen/video' exact ><Video age={18} /></Route>
         <Route path='/fourteenToEighteen/books' exact ><Books age={18} /></Route>
         <Route path='/fourteenToEighteen/career' exact ><Career age={18} /></Route>
         <Route path='/fourteenToEighteen/moral' exact ><Moral age={18} /></Route>
-        <Route component={NotFound} />
+        <Route path='/fourteenToEighteen/' component={FourteenToEighteen} exact />
+
+        <Route path='/fun-edu/' component={Home} />
+        <Route path='/' component={Home} exact />
       </Switch>
     </div>
   )
